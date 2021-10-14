061a9e9e82c251e2aab862fc05d79ee63b8afd24
joseluislemus <jllemus@gmail.com>
2021-10-14 09:28:47 +0200

    test

--- esphome/components/mcp3208/mcp3208.cpp
+++ esphome/components/mcp3208/mcp3208.cpp
@@ -24,10 +24,10 @@ float MCP3208::read_data(uint8_t pin) {
 //  uint8_t command = ((0x01 << 7) |          // start bit
 //                     ((pin & 0x07) << 4));  // channel number
 
-  uint8_t command1 = ((0x01 << 2) |          // start bit
-                     ((pin & 0x04) >> 2));  // channel number
+  uint8_t command1 = ((0d00010101 << 2) |          // start bit
+                     ((pin & 0d00000100) >> 2));  // channel number
 
-  uint8_t command2 = (pin & 0x03) << 6;
+  uint8_t command2 = (pin & 0d00000011) << 6;
 
   this->enable();
 //  this->transfer_byte(0x01);
